b174c00c3938a2f99b4c6049eec87c03a2680a85
alu0101203003 <alu0101203003@ull.edu.es>
2021-03-09 11:17:51 +0000

    estructura inicial finalizada

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -1,4 +1,12 @@
-function prueba (s :string){
+/**
+ * Funcion de prueba
+ * @param s Cadena de entrada
+ * @returns Devuelve la cadena
+ * ```ts
+ * prueba(prueba)
+ * ```
+ */
+export function prueba (s :string){
     return s;
 }
 console.log(prueba("prueba"));
