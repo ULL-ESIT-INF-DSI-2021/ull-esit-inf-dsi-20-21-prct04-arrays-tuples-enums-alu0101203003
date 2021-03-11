5e5be2b2bcaffe64ca7218736b8d828651ac0116
alu0101203003 <alu0101203003@ull.edu.es>
2021-03-11 09:30:41 +0000

    correccion ej1

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -8,14 +8,14 @@
  */
 
 export function decodeResistor (resistor :string[]){
-    type tupla = (string | number);
-    let tupla_code :tupla[][] = [["Negro",0],["Marron",1],["Rojo",2],["Naranja",3],["Amarillo",4],["Verde",5],["Azul",6],["Violeta",7],["Gris",8],["Blanco",9]]
+    type par = (string | number);
+    let codigosColor :par[][] = [["Negro",0],["Marron",1],["Rojo",2],["Naranja",3],["Amarillo",4],["Verde",5],["Azul",6],["Violeta",7],["Gris",8],["Blanco",9]]
     var code :string ="";
     var i :number = 0;
     var j :number = 0;
-    while(i < tupla_code.length){
-        if (tupla_code[i][0] == resistor[j]){
-            var codigo = tupla_code[i][1].toString()
+    while(i < codigosColor.length){
+        if (codigosColor[i][0] == resistor[j]){
+            var codigo = codigosColor[i][1].toString()
             code += codigo
             j++
             i = -1
